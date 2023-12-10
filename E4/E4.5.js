e71e92554970562fc90344fba5869167c7b717be
GennadyIG <gavrilenkogi@mail.ru>
2023-12-10 23:42:13 +0300

    add E4.5

---
+++ E4/E4.5.js
@@ -0,0 +1,44 @@
+class ElectricalAppliance {
+    constructor(name) {
+        this.name = name
+        this.status = false
+    }
+
+    turnOn() {
+        this.status = true
+    }
+
+    turnOff() {
+        this.status = false
+    }
+
+}
+
+class DeskLamp extends ElectricalAppliance {
+    constructor(name, color) {
+        super(name);
+        this.color = color
+    }
+
+    getInfo() {
+        return console.log(`${this.color} ${this.name} сейчас ${this.status ? 'включена' : 'выключена'}`)
+    }
+}
+
+class Desktop extends ElectricalAppliance {
+    constructor(name) {
+        super(name);
+    }
+
+    getInfo() {
+        return console.log(`${this.name} сейчас ${this.status ? 'включен' : 'выключен'}`)
+    }
+}
+
+
+const greenLamp = new DeskLamp('лампа', 'зеленая')
+greenLamp.turnOn()
+greenLamp.getInfo()
+
+const homeDesktop = new Desktop('Домашний компьютер')
+homeDesktop.getInfo()
